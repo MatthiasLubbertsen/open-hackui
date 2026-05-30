a1e2ab9ad0e0acf226cd41cc765860bc405d2a83
Matthias Lubbertsen <matthiaslubbertsen@gmail.com>
2026-05-30 17:54:17 +0200

    data.directConnections

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/webhook', (req, res) => {
       }
     }).then(res => res.json())
       .then(data => {
-        console.log('User settings:', data);
+        console.log('User settings:', data.directConnections);
         const oldSettings = data;
       })
       .catch(err => {
